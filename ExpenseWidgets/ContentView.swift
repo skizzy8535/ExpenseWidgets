ece8d60f1a121f9ea6a4e34094f9c7e453b9d2da
skizzy8535 <smirkiiiv@gmail.com>
2024-05-03 16:59:50 +0800

    Initial Commit

---
+++ ExpenseWidgets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseWidgets
+//
+//  Created by NeferUser on 2024/5/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenseWidgets/ExpenseWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseWidgetsApp.swift
+//  ExpenseWidgets
+//
+//  Created by NeferUser on 2024/5/3.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
